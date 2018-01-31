d7c997714fe1b8abce836774160123394cf6a8c5
Samuel Beaulieu <sambeau@bu.edu>
2018-01-31 12:56:09 -0500

    Updated temp

--- AnalogInput.py
+++ AnalogInput.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 import mraa
+from math import log
+
+B = 4275.0
+R0 = 100000.0
+
+def analog_to_celcius(value):
+	R = 1023.0/value - 1.0
+	R *= R0
+
+	temp = 1.0 / (log(R / R0)/B + 1.0/298.15) - 273.15
+
+	return temp
  
 try:
 	tempSensor = mraa.Aio(1)
-	print (tempSensor.read()), "is the current temperature"
+	temp = analog_to_celcius(tempSensor.read())
+	print (temp), "is the current temperature"
 except KeyboardInterrupt:
 	exit
